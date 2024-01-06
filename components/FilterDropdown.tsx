67b3c48daecbd1381dbe50505193badb4b6d786f
yensen-arch <officaialdevain25@gmail.com>
2024-01-06 15:57:23 +0530

    ZonalBillDue component added

---
+++ components/FilterDropdown.tsx
@@ -0,0 +1,62 @@
+"use client"
+import React, { useState } from "react";
+import Link from "next/link";
+
+const FilterDropdown = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+  return (
+    <div className="relative inline-block">
+      <button
+        className="flex mx-3 px-3 py-1 border border-solid text-lg rounded-xl bg-white hover:bg-slate-300 border-black  ml-auto"
+        onClick={() => setDropdownOpen(!isDropdownOpen)}
+      >
+        Filter
+        {isDropdownOpen ? (
+                  <img
+                    src="upArrow.svg"
+                    alt="Close Icon"
+                    className="h-7 mr-0"
+                  />
+                ) : (
+                  <img
+                    src="downArrow.svg"
+                    alt="Open Icon"
+                    className="h-7 mr-0"
+                  />
+                )}
+      </button>
+
+      {/* Dropdown Menu */}
+      {isDropdownOpen && (
+        <div className="absolute rounded-md bg-white ring-1 ring-black ring-opacity-5 ">
+          <ul>
+            <Link
+              href="#"
+              className="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100  border-solid "
+              role="menuitem"
+            >
+              Paid
+            </Link>
+            <Link
+              href="#"
+              className="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100  border-solid "
+              role="menuitem"
+            >
+              Due
+            </Link>
+            <Link
+              href="#"
+              className="block px-2 py-2 text-sm text-gray-700 hover:bg-gray-100  border-solid "
+              role="menuitem"
+            >
+              Industrial zone
+            </Link>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default FilterDropdown;
