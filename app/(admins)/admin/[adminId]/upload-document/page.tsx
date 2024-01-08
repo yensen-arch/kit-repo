6ce069851a7b34cd72e16aa70d3eb6792b56b0c4
yensen-arch <officaialdevain25@gmail.com>
2024-01-08 19:29:46 +0530

    master admin bill & doc pages styled

--- app/(admins)/admin/[adminId]/upload-document/page.tsx
+++ app/(admins)/admin/[adminId]/upload-document/page.tsx
@@ -3,7 +3,7 @@ import UploadDocMaster from "@/components/UpdoadDocMaster";
 // Master Admin Document Upload Page
 export default function BillDue({params}) {
     return (
-      <div className='bg-gray-100 min-h-screen p-3'>
+      <div className='bg-gray-100 min-h-screen p-8'>
       <UploadDocMaster/>
       </div>
     )
